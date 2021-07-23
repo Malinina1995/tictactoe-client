c26c44e43acfadb724643aa7a01851be5356ef63
malinina <alenam2803@mail.ru>
2021-07-24 00:08:40 +0300

    серверная игра

--- src/components/TikTakToeServerGame.ts
+++ src/components/TikTakToeServerGame.ts
@@ -1,17 +1,26 @@
 import {GameSocket} from "./game-socket";
 import {AbstractTicTacToeGame} from "./TicTacToeGame";
+import {GameSymbol} from "./GameSymbol";
+import {GameResult} from "./GameResult";
 
 export class TikTakToeServerGame extends AbstractTicTacToeGame {
+
     constructor(private gameSocket: GameSocket) {
         super();
         gameSocket.start();
+        gameSocket.subscribeOnFieldChange((squares: GameSymbol[]): void => {
+            this.notifyFieldChange(squares);
+        });
+        gameSocket.subscribeOnFinishGame((winner: GameResult): void => {
+            this.notifyResult(winner);
+        });
     }
 
     start(): void {
-// новая игра
+        this.gameSocket.startNewGame();
     }
 
     setStep(index: number): void {
-
+        this.gameSocket.sendStep(index);
     }
 }
--- src/components/game-socket.ts
+++ src/components/game-socket.ts
@@ -1,13 +1,41 @@
 import {io, Socket} from "socket.io-client";
 import {DefaultEventsMap} from "socket.io-client/build/typed-events";
+import {FieldChangeHandler, ResultHandler} from "./TicTacToeGame";
+import {GameSymbol} from "./GameSymbol";
+import {GameResult} from "./GameResult";
 
 export class GameSocket {
-    private socket: Socket<DefaultEventsMap, DefaultEventsMap>;
-    constructor(wsAddress: string = "ws://localhost:3001") {
-        this.socket = io(wsAddress, {reconnection: false});
+    private socket?: Socket<DefaultEventsMap, DefaultEventsMap>;
+    private fieldChangeHandler?: FieldChangeHandler;
+    private resultHandler?: ResultHandler;
+
+    constructor(private wsAddress: string = "ws://localhost:3001") {
     }
 
     start() {
-        this.socket.on('connect', () => {})
+        this.socket = io(this.wsAddress, {reconnection: false});
+        this.socket.on('connect', () => {});
+        this.socket.on('fieldChange', (squares: GameSymbol[]) => {
+            this.fieldChangeHandler && this.fieldChangeHandler(squares);
+        });
+        this.socket.on('finishGame', (winner: GameResult) => {
+            this.resultHandler && this.resultHandler(winner);
+        });
+    }
+
+    startNewGame() {
+        this.socket?.emit('startNewGame');
+    }
+
+    sendStep(index: number): void {
+        this.socket?.emit('step', index);
+    }
+
+    subscribeOnFieldChange(handler: FieldChangeHandler) {
+        this.fieldChangeHandler = handler;
+    }
+
+    subscribeOnFinishGame(handler: ResultHandler) {
+        this.resultHandler = handler;
     }
 }
